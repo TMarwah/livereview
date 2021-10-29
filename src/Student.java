926b6d31e7103ff56c11cfa70b30e2102fcfd099
tmarwah <marwah.tanmay@gmail.com>
2021-10-28 21:29:07 -0700

    finalized cli

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 public class Student {
     private String nameFirst;
     private String nameLast;
@@ -22,7 +23,7 @@ public class Student {
         this.grade = input.nextLine();
 
         idset();
-        System.out.println(nameFirst + nameLast + grade + studentid );
+        System.out.println( );
 
 
     }
@@ -33,8 +34,44 @@ public class Student {
         this.studentid =  grade + "" + id;
     }
     //add course
+    public void enroll() {
+        do{
+            System.out.print("Enter courses you would like to enroll in: ");
+            Scanner input = new Scanner(System.in);
+            String course = input.nextLine();
 
+            //exit loop case
+            if (!course.equals("q")){
+                courses = courses + "\n" + course;
+                balance = balance + cost;
+            }
+            else{
+                System.out.println("Leaving loop \n");
+                break;
+            }
+        } while (1 != 0);
+
+        System.out.println("You are enrolled in: " + courses);
+        System.out.println("Total cost of tuition $" + balance);
+    }
     //course cost checking
+    public void costCheck(){
 
+        System.out.println("Your remaining balance is $" + balance);
+    }
+
+    //pay tuition
+    public void payCost(){
+        System.out.print("Enter your payment: ");
+        Scanner input = new Scanner(System.in);
+        int payment = input.nextInt();
+        balance = payment - balance;
+        System.out.println("Your account funds are $" + payment);
+        costCheck();
+    }
     //finalize details
+    public void finalAccount() {
+        System.out.println("Name: " + nameFirst + " " + nameLast + "\n" + "Grade: " + grade + "\nStudent ID: " + nameFirst + nameLast + grade + studentid + "\n"
+                + "Enrolled Courses: " + courses + "\n" + "Account balance: $" + balance);
+    }
 }
--- src/studentdb.java
+++ src/studentdb.java
@@ -1,7 +1,18 @@
+import java.util.concurrent.TimeUnit;
+
 public class studentdb {
 
     public static void main(String[] args){
          Student stu1 = new Student();
+         stu1.enroll();
+         stu1.payCost();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 50; ++i) System.out.println();
+        stu1.finalAccount();
         //prompt for student number
 
         //creates students
